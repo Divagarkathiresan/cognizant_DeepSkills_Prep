6d950bb5520f91e95e31fc306460167282adf36f
Sanjana <sanjana@syn-172-100-128-241.res.spectrum.com>
2026-06-16 15:04:20 +0530

    Scenario 2 Control structres completed

--- PL SQL/test.sql
+++ PL SQL/test.sql
@@ -1,20 +1,22 @@
-create or replace function generate_id
-return number 
-as 
-max_id number;
-BEGIN
-    select max(ID) into max_id from Student;
-    return max_id+1;
-end;
-/
+-- create or replace function generate_id
+-- return number 
+-- as 
+-- max_id number;
+-- BEGIN
+--     select max(ID) into max_id from Student;
+--     return max_id+1;
+-- end;
+-- /
 
-DECLARE
-    v_id Student.ID%Type;
-BEGIN
-    v_id:=generate_id;
-    insert into Student values(v_id , 'Student_' || v_id);
-end;
-/
+-- DECLARE
+--     v_id Student.ID%Type;
+-- BEGIN
+--     v_id:=generate_id;
+--     insert into Student values(v_id , 'Student_' || v_id);
+-- end;
+-- /
 
 
-select * FROM Student;
+-- select * FROM Student;
+
+select * from Customers;
