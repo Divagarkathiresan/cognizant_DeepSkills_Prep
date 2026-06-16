5197e3d095b3c4ddc30a86b4930e253ef63307ff
Sanjana <sanjana@syn-172-100-128-241.res.spectrum.com>
2026-06-16 14:17:28 +0530

    Pl/Sql check completed

--- PL SQL/test.sql
+++ PL SQL/test.sql
@@ -1,11 +1,20 @@
-create or replace function get_square(
-    num number
-)
-return number
-as
-begin
-    return num * num;
+create or replace function generate_id
+return number 
+as 
+max_id number;
+BEGIN
+    select max(ID) into max_id from Student;
+    return max_id+1;
 end;
 /
 
-select get_square(5) as square_of_5 from dual;
+DECLARE
+    v_id Student.ID%Type;
+BEGIN
+    v_id:=generate_id;
+    insert into Student values(v_id , 'Student_' || v_id);
+end;
+/
+
+
+select * FROM Student;
