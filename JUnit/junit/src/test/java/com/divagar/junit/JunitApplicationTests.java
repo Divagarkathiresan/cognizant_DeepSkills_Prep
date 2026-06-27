7ed3b3e8ce145ab2cfcb4544d046643d1e3b4412
Sanjana <sanjana@Sanjanas-MacBook-Air.local>
2026-06-27 17:00:26 +0530

    JUnit Started

---
+++ JUnit/junit/src/test/java/com/divagar/junit/JunitApplicationTests.java
@@ -0,0 +1,14 @@
+package com.divagar.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
