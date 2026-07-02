c291d7e87df2100625f8a86ccbbd0ebae6595e92
Sanjana <sanjana@Sanjanas-MacBook-Air.local>
2026-07-02 17:39:37 +0530

    Module 5 Started

---
+++ Maven(Module5)/divagar/src/main/java/com/example/divagar/DivagarApplication.java
@@ -0,0 +1,13 @@
+package com.example.divagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivagarApplication.class, args);
+	}
+
+}
