9711abc0cb8c8681d0240807222a1d6344ec151a
Sanjana <sanjana@Sanjanas-MacBook-Air.local>
2026-07-23 17:47:18 +0530

    Practice : SpringBoot 3 HandsOn second exercise cmpleted

--- Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,14 +1,23 @@
 package com.cognizant.spring_learn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-		System.out.println("Main method executed !!!");
-	}
+    public static void main(String[] args) {
+        displayCountry();
+    }
 
+    public static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        System.out.println("Country : " + country);
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
 }
