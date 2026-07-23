031b9e874527d563016e689af93ccc763463534d
Sanjana <sanjana@Sanjanas-MacBook-Air.local>
2026-07-23 18:07:30 +0530

    Practice : SpringBoot 3 Han dsOn 4th Exercise implemented

--- Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/Controllers/CountryController.java
+++ Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/Controllers/CountryController.java
@@ -2,11 +2,19 @@ package com.cognizant.spring_learn.Controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.Country;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
+@RequestMapping("/country")
 public class CountryController {
     public final static Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
     @GetMapping("/hello")
@@ -15,5 +23,23 @@ public class CountryController {
         String message = "Hello World!!";
         LOGGER.info("END - sayHello()");
         return message;
-    } 
+    }
+
+    @GetMapping
+    public Country getCountryIndia() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.info("END");
+
+        ((ClassPathXmlApplicationContext) context).close();
+
+        return country;
+    }
 }
--- Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Spring_REST_using_Spring_Boot_3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -3,9 +3,11 @@ package com.cognizant.spring_learn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
+@ImportResource("classpath:country.xml")
 public class SpringLearnApplication {
 
     // public static void main(String[] args) {
